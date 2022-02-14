72b83f06490cbb14e1139ad3a1209c4f80baf065
Bayard <bayardtarpley@gmail.com>
2022-02-14 16:26:05 +0000

    Updating with endpoint for Article ID search, error handling,  and tests

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const { getTopics } = require('./controllers/topics.controllers')
+const { getTopics } = require("./controllers/topics.controllers");
+const { getArticleById } = require("./controllers/articles.controllers");
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Error 404 - Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    res.status(500).send({ msg: "Internal Server Error" });
+    console.log(err);
+  }
 });
 
 module.exports = app;
---
+++ models/articles.models.js
@@ -0,0 +1,18 @@
+const db = require("../db/connection");
+
+exports.selectArticleById = (requestedArticleId) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [
+      requestedArticleId,
+    ])
+    .then(({ rows }) => {
+      const article = rows[0];
+      if (!article) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${requestedArticleId}`,
+        });
+      }
+      return article;
+    });
+};
