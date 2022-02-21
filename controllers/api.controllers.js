45697ee89aa3d98e3ec4e6ae97539c8367d898d0
Bayard <bayardtarpley@gmail.com>
2022-02-21 15:24:56 +0000

    Updating with /api endpoint and testing.

--- controllers/api.controllers.js
+++
@@ -1,9 +0,0 @@
-const { getApiEndpoints } = require("../models/api.models");
-
-exports.getApi = (req, res, next) => {
-  getApiEndpoints()
-    .then((endpoints) => {
-      res.status(200).send({ endpoints });
-    })
-    .catch(next);
-};
--- models/api.models.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs/promises");
-
-exports.getApiEndpoints = () => {
-  return fs.readFile("./endpoints.json").then((jsonEndpoints) => {
-    const endpoints = JSON.parse(jsonEndpoints)
-    return endpoints;
-  });
-};
