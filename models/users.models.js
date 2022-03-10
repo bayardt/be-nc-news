054eb423be3cd43f6d5200478a19d32b179bad1e
Bayard <bayardtarpley@gmail.com>
2022-03-10 11:50:40 +0000

    troubleshooting user query

--- models/users.models.js
+++ models/users.models.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
-exports.selectUsers = (authorUsername) => {
-    console.log(authorUsername = null)
+exports.selectUsers = ({authorUsername} = null) => {
+    console.log(authorUsername)
     if (!authorUsername) return db.query("SELECT * FROM users;").then(({ rows }) => rows);
     return db.query(`SELECT * FROM users WHERE username = '${authorUsername}';`).then(({ rows }) => rows);    
 };
